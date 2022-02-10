d34a63930d401f7b5d9ba1a0fd3938395ba7b59c
unknown <kleiton.batista@catolica.edu.br>
2022-02-09 23:28:06 -0300

    projeto to do list iniciado

---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Item } from "../../types/Item";
+import * as C from "./styles";
+
+type Props = {
+  item: Item;
+};
+export const ListItem = ({ item }: Props) => {
+  const [isChecked, setIsChecked] = useState(item.done);
+
+  return (
+    <C.Container done={isChecked}>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={(e) => setIsChecked(e.target.checked)}
+      />
+      <label>{item.name} - {item.done.toString()}</label>
+    </C.Container>
+  );
+};
